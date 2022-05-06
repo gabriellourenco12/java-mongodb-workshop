0b6b57c025302a4ea6a8c9b63641073108331804
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-05-06 00:12:21 -0300

    initial commit

---
+++ src/main/java/application/JavaMongodbWorkshopApplication.java
@@ -0,0 +1,13 @@
+package javamongodbworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMongodbWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMongodbWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/application/JavaMongodbWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package javamongodbworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMongodbWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
