77031cfb92899aa72772511c0c4160e704ea2ef0
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-06-06 20:25:48 -0300

    Entity User e REST funcionando

---
+++ src/main/java/application/domain/User.java
@@ -0,0 +1,43 @@
+package application.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+    private String email;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            return other.id == null;
+        }
+        return id.equals(other.id);
+    }
+}
