5bbb4ff69b4ccdfb736cf89474a41d2d4ba1dc3e
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-06-07 00:21:52 -0300

    consulta com varios criterios

--- src/main/java/application/resources/util/URL.java
+++ src/main/java/application/resources/util/URL.java
@@ -1,12 +1,25 @@
 package application.resources.util;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
     public static String decodeParam(String text) {
         return URLDecoder.decode(text, StandardCharsets.UTF_8);
+    }
+
+    public static Date convertDate(String textDate, Date defaultValue) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        try {
+            return sdf.parse(textDate);
+        } catch (ParseException e) {
+            return defaultValue;
+        }
     }
 }
