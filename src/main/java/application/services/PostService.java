16c8b31878851e429ace94e7374dd67aa5b6afa9
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-06-06 23:33:10 -0300

    obtendo um post por id

---
+++ src/main/java/application/services/PostService.java
@@ -0,0 +1,21 @@
+package application.services;
+
+import application.domain.Post;
+import application.repository.PostRepository;
+import application.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
