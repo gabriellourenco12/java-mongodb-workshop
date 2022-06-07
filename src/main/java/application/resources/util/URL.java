60cb93967b5aea83d421010cae834362bb2f2758
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-06-06 23:55:54 -0300

    consulta simples com query methods

---
+++ src/main/java/application/resources/util/URL.java
@@ -0,0 +1,12 @@
+package application.resources.util;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+
+public class URL {
+
+    public static String decodeParam(String text) {
+        return URLDecoder.decode(text, StandardCharsets.UTF_8);
+    }
+}
