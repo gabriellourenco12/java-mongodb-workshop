cf0b9275e948e9ff1c252239165f28d56895ace8
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-06-06 21:37:15 -0300

    usando padrao DTO para retornar usuarios

---
+++ src/main/java/application/dto/UserDTO.java
@@ -0,0 +1,25 @@
+package application.dto;
+
+import application.domain.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+    private String email;
+
+    public UserDTO(User obj) {
+        id = obj.getId();
+        name = obj.getName();
+        email = obj.getEmail();
+    }
+}
--- src/main/java/application/resources/UserResource.java
+++ src/main/java/application/resources/UserResource.java
@@ -1,6 +1,7 @@
 package application.resources;
 
 import application.domain.User;
+import application.dto.UserDTO;
 import application.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -18,8 +20,9 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method=RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(UserDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
